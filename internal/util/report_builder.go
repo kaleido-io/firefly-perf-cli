cad1386bc16f7ece3a2dab6ffc59558ca2944b6f
Chengxuan Xing <chengxuan.xing@kaleido.io>
2023-11-14 14:53:19 +0000

    remove unused stylesheets
    Signed-off-by: Chengxuan Xing <chengxuan.xing@kaleido.io>

--- internal/util/report_builder.go
+++ internal/util/report_builder.go
@@ -34,8 +34,6 @@ func (r *Report) GenerateHTML() error {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Firefly: Performance Report</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-        <link rel="stylesheet" href="assets/styles.css">
         <style>
             .main {
                 background: #fff;
